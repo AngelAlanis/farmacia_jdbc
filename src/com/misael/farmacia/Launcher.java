6c8d4f3472a263f15993f37f5dcf29437a84a140
misael.alanis <ahangel256@gmail.com>
2022-11-05 12:38:54 -0600

    Avance interfaz ventana principal

--- src/com/misael/farmacia/Launcher.java
+++ src/com/misael/farmacia/Launcher.java
@@ -1,11 +1,12 @@
 package com.misael.farmacia;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 
 public class Launcher {
 
     public static void main(String[] args) {
-        FlatLightLaf.setup();
+        FlatDarkLaf.setup();
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
 
     }
--- src/com/misael/farmacia/VentanaPrincipal.java
+++ src/com/misael/farmacia/VentanaPrincipal.java
@@ -8,8 +8,11 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JToolBar;
+import javax.swing.table.DefaultTableModel;
 
 public class VentanaPrincipal extends JFrame {
     private JPanel      panelPrincipal;
@@ -24,16 +27,30 @@ public class VentanaPrincipal extends JFrame {
     private JLabel      iconoLogo;
     private JPanel      panelUsuario;
     private JLabel      labelLeAtiende;
-    private JButton btnUsuario;
-    private JButton btnBuscar;
-    private JButton btnEliminar;
-    private JButton btnCobrar;
-    private JLabel  labelCantidadProductos;
-    private JLabel labelCostoTotal;
-    private JToolBar trailing;
+    private JButton     btnUsuario;
+    private JButton     btnBuscar;
+    private JButton     btnEliminar;
+    private JButton     btnCobrar;
+    private JLabel      labelCantidadProductos;
+    private JLabel      labelCostoTotal;
+    private JTable      tableVenta;
+    private JScrollPane spVenta;
+    private JTable      tablaInventario;
+    private JScrollPane spInventario;
+    private JTable      tablaProveedores;
+    private JScrollPane spProveedores;
+    private JTable      tablaHistorialVentas;
+    private JScrollPane spHistorialVentas;
+    private JTable      tablaAbastecimientos;
+    private JScrollPane spAbastecimientos;
+    private JToolBar    trailing;
     private JButton     btnSalir;
     private ImageIcon   iconoSalir;
-    private ImageIcon   imagenLogo;
+    private ImageIcon   iconoVenta;
+    private ImageIcon   iconoInventario;
+    private ImageIcon   iconoProveedores;
+    private ImageIcon   iconoHistorialVentas;
+    private ImageIcon   iconoAbastecimientos;
     private Utilidades  utilidades = new Utilidades();
 
 
@@ -42,11 +59,16 @@ public class VentanaPrincipal extends JFrame {
     }
 
     public void inicializarIconos() {
-        iconoSalir = utilidades.imageIcon("/icons/salir.png");
-        imagenLogo = utilidades.imageIcon("/logoFarmacia.png");
+        iconoSalir           = utilidades.imageIcon("/icons/salir.png");
+        iconoVenta           = utilidades.imageIcon("/icons/ventas.png");
+        iconoInventario      = utilidades.imageIcon("/icons/inventario.png");
+        iconoProveedores     = utilidades.imageIcon("/icons/proveedor.png");
+        iconoHistorialVentas = utilidades.imageIcon("/icons/registro.png");
+        iconoAbastecimientos = utilidades.imageIcon("/icons/abastecimiento.png");
     }
 
     public void configurarComponentes() {
+        //Botón de salir en el tabbedPane
         trailing = new JToolBar();
         trailing.setFloatable(false);
         trailing.setBorder(null);
@@ -56,7 +78,16 @@ public class VentanaPrincipal extends JFrame {
         trailing.add(btnSalir);
         tabbedPane.putClientProperty(FlatClientProperties.TABBED_PANE_TRAILING_COMPONENT, trailing);
 
-        iconoLogo.setIcon(imagenLogo);
+        //Modelo default de la tabla de Ventas
+        String[] columnasVenta = {"Folio", "Descripción del producto", "Precio de venta", "Cantidad", "Importe", "Existencia"};
+        tableVenta.setModel(new DefaultTableModel(null, columnasVenta));
+
+        //Iconos de las pestañas del tabbedPane
+        tabbedPane.setIconAt(0, iconoVenta);
+        tabbedPane.setIconAt(1, iconoInventario);
+        tabbedPane.setIconAt(2, iconoProveedores);
+        tabbedPane.setIconAt(3, iconoHistorialVentas);
+        tabbedPane.setIconAt(4, iconoAbastecimientos);
     }
 
     public VentanaPrincipal() {
